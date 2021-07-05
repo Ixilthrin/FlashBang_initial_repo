501dd0607c520133f62f0b27e14cf46f2c41f113
ixilthrin <dgstover@gmail.com>
2021-07-05 15:54:00 -0700

    Added new class ImageReader.

---
+++ code/general/ImageReader.cpp
@@ -0,0 +1,51 @@
+#include "ImageReader.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+ImageReader::ImageReader(std::string filePath)
+{
+	_filePath = filePath;
+	_width = 0;
+	_height = 0;
+	_componentCount = 0;
+	_dataWasLoaded = false;
+}
+
+ImageReader::~ImageReader()
+{
+	if (_dataWasLoaded)
+	{
+		stbi_image_free(_imageData);
+	}
+}
+
+void ImageReader::LoadData()
+{
+	_imageData = stbi_load(_filePath.c_str(), &_width, &_height, &_componentCount, STBI_rgb);
+	_dataWasLoaded = true;
+}
+
+GLubyte* ImageReader::getImageData()
+{
+	if (!_dataWasLoaded)
+	{
+		LoadData();
+	}
+	return _imageData;
+}
+
+int ImageReader::getWidth()
+{
+	return _width;
+}
+
+int ImageReader::getHeight()
+{
+	return _height;
+}
+
+int ImageReader::getComponentCount()
+{
+	return _componentCount;
+}
---
+++ code/general/ImageReader.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <string>
+
+class ImageReader
+{
+private:
+	std::string _filePath;
+	GLubyte* _imageData;
+	void LoadData();
+	int _height;
+	int _width;
+	int _componentCount;
+	bool _dataWasLoaded;
+public:
+	ImageReader(std::string filePath);
+	~ImageReader();
+	GLubyte* getImageData();
+	int getWidth();
+	int getHeight();
+	int getComponentCount();
+};
